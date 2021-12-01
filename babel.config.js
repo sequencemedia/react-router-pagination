9676f6f6ae2feda23d6f7f21c34743b3f3cbdf0c
Jonathan Perry <jonathanperry@sequencemedia.net>
2021-12-01 00:23:13 +0000

    Babel

--- babel.config.js
+++ babel.config.js
@@ -22,7 +22,7 @@ const presets = [
   [
     '@babel/env', {
       targets: {
-        node: '12.19.0',
+        node: 'current',
         browsers: [
           'last 4 versions',
           'safari >= 9',
