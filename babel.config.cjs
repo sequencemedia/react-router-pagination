a8d7931a1d8e6e7d2330b17efbf933bf6b3c3751
Jonathan Perry <jonathanperry@sequencemedia.net>
2024-12-08 13:23:54 +0000

    Removes module resolver

--- babel.config.cjs
+++ babel.config.cjs
@@ -38,25 +38,10 @@ const presets = [
   ]
 ]
 
-const plugins = [
-  [
-    'module-resolver',
-    {
-      alias: {
-        '#pagination/centered': './src/pagination/centered/index.tsx',
-        '#pagination/standard': './src/pagination/standard/index.tsx',
-        '#pagination/component': './src/pagination/component.tsx',
-        '#pagination/index': './src/pagination/index.tsx'
-      }
-    }
-  ]
-]
-
 module.exports = (api) => {
   if (api) api.cache.using(env)
 
   return {
-    presets,
-    plugins
+    presets
   }
 }
